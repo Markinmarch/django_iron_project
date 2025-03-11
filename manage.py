9e652fdbf7aa2653e2ef61f5343c7083c70223cd
Markinmarch <iztvmrk@mail.ru>
2025-03-11 20:59:23 +0300

    rebuild project to forum online

--- manage.py
+++ manage.py
@@ -2,28 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import logging
 
-def logger():
-    if 'iron_project.log' not in os.listdir('.'):
-        with open('iron_project.log', mode = 'x', encoding = 'utf-8') as log_file:
-            log_file.close()
-    else:
-        pass
-
-    logging.basicConfig(
-        format = '%(asctime)s %(name)s %(levelname)s %(message)s',
-        level = logging.INFO,
-        handlers = [
-            logging.FileHandler('iron_project.log'),
-            logging.StreamHandler()
-        ]
-    )
-    
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IRON.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -36,7 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # from run_db import run_DB
-    # run_DB
     main()
-    logger()
